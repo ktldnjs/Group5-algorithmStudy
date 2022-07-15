c22e9fd2ea139a398b9045c310f73501ef312154
Jungmin-Noh <lumill925@gmail.com>
2022-07-15 18:55:01 +0900

    3번 문제

---
+++ algorithm/algo3.java
@@ -0,0 +1,14 @@
+# 가운데 글자 가져오기
+
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+
+        if(s.length()%2 == 0){
+            answer = s.substring((s.length()/2)-1, (s.length()/2)+1);
+        }else{
+            answer = s.substring((s.length()/2), (s.length()/2)+1);
+        }
+        return answer;
+    }
+}
