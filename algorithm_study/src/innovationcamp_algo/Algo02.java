d5f3ffec68a751b5bdaa67401474ac80b2cdc002
HASEUNGHEEE <ariul123@gmail.com>
2022-07-15 20:40:48 +0900

    2번 문제

---
+++ algorithm_study/src/innovationcamp_algo/Algo02.java
@@ -0,0 +1,9 @@
+package innovationcamp_algo;
+
+public class Algo02 {
+    public String Algo02(int num) {
+        String answer = "";
+        answer = (num % 2 == 0) ? "Even" : "Odd";
+        return answer;
+    }
+}
